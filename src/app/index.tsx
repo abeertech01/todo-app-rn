e81e6183d420419d43da91cf543ccc65ef01e016
abeertech01 <abeer.technology@gmail.com>
2026-09-05 02:10:44 +0600

    moment of learning

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,13 @@
-import { Text, View, StyleSheet } from "react-native"
+import { Link } from "expo-router"
+import { StyleSheet, Text, View } from "react-native"
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen</Text>
+      <Text style={styles.content}>
+        Edit src/app/index.tsx to edit this screen
+      </Text>
+      <Link href="/about">Visit about screen</Link>
     </View>
   )
 }
@@ -13,5 +17,8 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+  },
+  content: {
+    fontSize: 20,
   },
 })
